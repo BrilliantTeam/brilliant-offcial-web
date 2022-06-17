590cdd5c8b1ba5bd1580c839419a09307730b5ac
RICE <68499464+RICE0707@users.noreply.github.com>
2022-06-17 20:35:29 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,6 +3,7 @@ hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net
     server: '762627112867725403',
     channel: '971746021262630962',
     avatar: 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
+    shard: 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
     glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png', '100%']
   })
 </script>`, 'default');
