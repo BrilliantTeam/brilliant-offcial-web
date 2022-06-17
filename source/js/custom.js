0ffa9358178a044b01c7c26d52c029f2a4fc425e
NCT-skyouo <56823143+NCT-skyouo@users.noreply.github.com>
2022-06-17 19:39:58 +0800

    Create custom.js

---
+++ source/js/custom.js
@@ -0,0 +1,7 @@
+hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+  new Crate({
+    server: '762627112867725403',
+    channel: '971746021262630962',
+    glyph: ['https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png', '100%']
+  })
+</script>`, 'default');
