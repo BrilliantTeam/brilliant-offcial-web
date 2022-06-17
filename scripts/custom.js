3e2dea3da6b861946087c21db50f974519283f4d
RICE <68499464+RICE0707@users.noreply.github.com>
2022-06-17 20:41:20 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,7 +1,7 @@
 hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
   new Crate({
     server: '762627112867725403',
-    channel: '971746021262630962',
+    channel: '914360488723382352',
     avatar: 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
     glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png', '100%']
   })
