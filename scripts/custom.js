fb2717303808e52b9b3dcc99a089770c2f21e220
RICE <68499464+RICE0707@users.noreply.github.com>
2022-06-17 20:02:19 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -4,6 +4,6 @@ hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net
     channel: '971746021262630962',
     username: '從官方網站發出的訊息',
     avatar: 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
-    glyph: ['https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png', '100%']
+    glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png', '100%']
   })
 </script>`, 'default');
