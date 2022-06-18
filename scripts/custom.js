cd469412646a0db53bf2efd317aaea68bb316907
RICE <68499464+RICE0707@users.noreply.github.com>
2022-06-18 12:46:10 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -8,7 +8,7 @@ hexo.extend.injector.register('body_end', <script src="https://cdn.jsdelivr.net/
   })
   setInterval(() => {
     button.options.glyph = ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png' + Date.now(), '100%']
-    button.notify(
+    button.notify({
       content: 'Background updated',
       timeout: 1000
   }, 1500)
