eb51060d422d8365c41a4db65ff8e592d53598f3
RICE <68499464+RICE0707@users.noreply.github.com>
2022-06-18 12:47:31 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,15 +1,37 @@
 hexo.extend.injector.register('body_end', <script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+
   const button = new Crate({
+
     server: '762627112867725403',
+
     channel: '914360488723382352',
+
     avatar:
+
 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
+
     glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png' + Date.now(), '100%']
+
   })
+
+  
+
+  // Random cat pictures every 1.5s
+
   setInterval(() => {
+
     button.options.glyph = ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png' + Date.now(), '100%']
+
+    
+
     button.notify({
+
       content: 'Background updated',
+
       timeout: 1000
+
+    })
+
   }, 1500)
+
 </script>
