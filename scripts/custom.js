a1b38faca68a40bfbaf0accf48d8cb28ad1a135f
RICE <68499464+RICE0707@users.noreply.github.com>
2022-06-18 12:54:33 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,16 +1,15 @@
 hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-  const button = new Crate({
-    server: '762627112867725403',
-    channel: '914360488723382352',
-    glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png' + Date.now(), '100%']
-  })
-  
-  setInterval(() => {
-    button.options.glyph = ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png' + Date.now(), '100%']
 
-    button.notify({
-      content: 'Background updated',
-      timeout: 1000
-    })
-  }, 1500)
+  new Crate({
+
+    server: '762627112867725403',
+
+    channel: '914360488723382352',
+
+    avatar: 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
+
+    glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png', '100%']
+
+  })
+
 </script>`, 'default');
