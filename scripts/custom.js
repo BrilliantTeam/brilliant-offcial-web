9ff1a5ffbb0b31d23c996844920c9f38bc96ebd0
NCT-skyouo <56823143+NCT-skyouo@users.noreply.github.com>
2022-06-22 19:32:48 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -8,4 +8,7 @@ hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net
 </script>`, 'default');
 
 hexo.extend.injector.register('head_begin', `<link rel="icon" href="https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png">`, 'default');
-hexo.extend.injector.register('head_end', `<meta property="og:thumbnail" content="https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png">`, 'default');
+hexo.extend.injector.register('head_end', `<meta property="og:thumbnail" content="https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png">
+<meta property="og:image:type" content="image/png">
+<meta property="og:image:width" content="256">
+<meta property="og:image:height" content="256">`, 'default');
