199e63a643f870fcbccecc9184af7fc70c0de03f
RICE <68499464+RICE0707@users.noreply.github.com>
2022-07-16 11:46:15 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,14 +1,1 @@
-hexo.extend.injector.register('body_end', `<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-  new Crate({
-    server: '762627112867725403',
-    channel: '914360488723382352',
-    avatar: 'https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png',
-    glyph: ['https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png', '100%']
-  })
-</script>`, 'default');
-
-hexo.extend.injector.register('head_begin', `<link rel="icon" href="https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png">`, 'default');
-hexo.extend.injector.register('head_end', `<meta property="og:image" content="https://media.discordapp.net/attachments/949275617520660490/986589107792773210/1653318260408.png">
-<meta property="og:image:type" content="image/png">
-<meta property="og:image:width" content="256">
-<meta property="og:image:height" content="256">`, 'default');
+;
