9acb8be897f323822dbbf3dbf0f1d58f763ced69
RICE <68499464+RICE0707@users.noreply.github.com>
2022-07-16 21:39:36 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,1 +1,5 @@
-;
+hexo.extend.injector.register('head_begin', `<link rel="icon" href="https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png">`, 'default');
+hexo.extend.injector.register('head_end', `<meta property="og:image" content="https://media.discordapp.net/attachments/949275617520660490/987323589185388575/AddText_09-19-10.11.17.png">
+<meta property="og:image:type" content="image/png">
+<meta property="og:image:width" content="256">
+<meta property="og:image:height" content="256">`, 'default')
