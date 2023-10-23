d7017dc6d9735c46ed095c193c306182e5b2b5ff
RiceChen_ <68499464+RICE0707@users.noreply.github.com>
2023-10-23 23:08:44 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,5 +1,5 @@
-hexo.extend.injector.register('head_begin', `<link rel="icon" href="https://cdn.discordapp.com/attachments/1084466431124570132/1104368155725406238/Untitled83_20230506181330.png">`, 'default');
-hexo.extend.injector.register('head_end', `<meta property="og:image" content="https://cdn.discordapp.com/attachments/1084466431124570132/1104368155725406238/Untitled83_20230506181330.png">
+hexo.extend.injector.register('head_begin', `<link rel="icon" href="https://www.brilliantw.net/img/輝煌icon.png">`, 'default');
+hexo.extend.injector.register('head_end', `<meta property="og:image" content="https://www.brilliantw.net/img/輝煌icon.png">
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="256">
 <meta property="og:image:height" content="256">`, 'default')
