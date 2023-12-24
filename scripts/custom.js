ebb51ee6227743364f5e535aaf1a0e394bbac3af
RiceChen_ <68499464+RICE0707@users.noreply.github.com>
2023-12-25 01:55:57 +0800

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,22 +3,3 @@ hexo.extend.injector.register('head_end', `<meta property="og:image" content="ht
 <meta property="og:image:type" content="image/png">
 <meta property="og:image:width" content="256">
 <meta property="og:image:height" content="256">`, 'default')
-
-window.addEventListener("mousemove", function (e) {
-	var to_append = document.getElementsByClassName('loader-container')[0];
-	var all = document.getElementsByClassName('loader-container');
-
-	var parent_div = document.createElement('div');
-	parent_div.className = "loader-container";
-	var inner_div = document.createElement('div');
-	inner_div.className = "loader";
-	parent_div.appendChild(inner_div)
-	var d = document.body.appendChild(parent_div);
-
-	parent_div.style.left = (e.clientX - 50)+'px';
-	parent_div.style.top = (e.clientY - 50)+'px';
-
-	if(document.getElementsByClassName('loader-container').length > 50) {
-		document.body.removeChild(to_append)
-	}
-});
